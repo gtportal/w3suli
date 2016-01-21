c4d1a2a9cfe36525b00d802af22758f344592639
gtportal <gtportal@gtportal-desktop>
2016-01-21 17:26:02 +0100

    w3suli_responsive.css belinkelve

--- public_html/index.php
+++ public_html/index.php
@@ -84,6 +84,7 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes">
      <link type="text/css" rel="stylesheet" media="all"   href="css/w3suli_alap.css" />  
      <link type="text/css" rel="stylesheet" media="all"   href="css/w3suli_szerkeszt.css" />  
+     <link type="text/css" rel="stylesheet" media="all"   href="css/w3suli_responsive.css" />
      <?php echo getHead(); ?>
   </head>
   <body>	  
