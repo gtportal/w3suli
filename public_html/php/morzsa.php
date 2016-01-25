f64dd38f0f3a45281c10e3d5fdd1aef4bbb2a615
hw6 <hw6@Hw6>
2016-01-25 15:16:07 +0100

    morzsa v1.2

--- public_html/php/morzsa.php
+++ public_html/php/morzsa.php
@@ -18,12 +18,12 @@ function getMorzsaHTML() {
            
            //Ha az adott oldal vagy annak egy leszármazottja aktív, akkor leszármazottjait is megjelenítjük
            if ($Aktiv) {
-               $HTMLkod .= Menu_Szint2($OID);
-            $HTMLkod .= "<a href='?f0=$OURL' >$ONev</a> | ";
+               $HTMLkod .= Morzsa_Szint2($OID);
+               $HTMLkod .= "<a href='?f0=$OURL' >$ONev</a> | ";
            }
            $HTMLkod .= "\n"; 
         } 
-        if ($HTMLkod > '') {$HTMLkod = "<ul class='Ul1'>\n $HTMLkod  </ul>\n";}
+        if ($HTMLkod > '') {$HTMLkod = "<div class='Morzsa'>\n $HTMLkod  </div>\n";}
         mysqli_free_result($result);  
         return $HTMLkod1.$HTMLkod;
     }
@@ -31,7 +31,30 @@ function getMorzsaHTML() {
     
     
     
-    
+        function Morzsa_Szint2($OID) {
+        global $Aktoldal, $SzuloOldal, $NagyszuloOldal, $MySqliLink, $DedSzuloId;
+        $HTMLkod      = '';
+        //Második szint 
+        $SelectStr   = "SELECT * FROM Oldalak WHERE OSzuloId=$OID order by ONev "; 
+        $result      = mysqli_query($MySqliLink,$SelectStr) OR die("Hiba OM 41 ");
+        while($row   = mysqli_fetch_array($result)) {
+            $ONev = $row['ONev']; $OURL = $row['OUrl']; $OID  = $row['id']; $OSzulo = $row['OSzuloId']; 
+           //Ha az adott oldal vagy annak első gyermeke aktív, akkor az 'AktLink' osztályba kerül
+           if ($OID==$Aktoldal['id']      || 
+               $OID==$SzuloOldal['id']    ||
+               $OID==$NagyszuloOldal['id'] ||
+               $OID==$DedSzuloId   
+              ) {$Aktiv = true;} else {$Aktiv = false;}           
+           //Ha az adott oldal vagy annak egy leszármazottja aktív, akkor leszármazottjait is megjelenítjük
+           if ($Aktiv) {                
+                $HTMLkod .= "<a href='?f0=$OURL' >$ONev</a>";
+                $HTMLkod .= Morzsa_Szint3($OID);
+           }
+           $HTMLkod .= "\n";           
+        }        
+        mysqli_free_result($result);  
+        return $HTMLkod;
+    }
     
     
 ?>
