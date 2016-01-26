046cd8104560969eb2c316ab3d767835710d4d5a
hw6 <hw6@Hw6>
2016-01-26 22:40:40 +0100

    Lábléc (lista megvalósításban css miatt)

---
+++ public_html/php/Lablec.php
@@ -0,0 +1,17 @@
+<?php
+    function getLablecHTML() {
+        $HTMLkod = "";
+        $AlapAdatok = getAlapbeallitasok();
+        $WebhelyNev = $AlapAdatok['WebhelyNev'];
+        $Iskola = $AlapAdatok['Iskola'];
+        $Cim = $AlapAdatok['Cim'];
+        $Telefon = $AlapAdatok['Telefon'];
+        
+        $HTMLkod .= "<ul id = 'footer_ul'>\n
+                    <li class = 'footer_li'>$WebhelyNev</li>\n
+                    <li class = 'footer_li'>$Iskola</li>\n
+                    <li class = 'footer_li'>$Cim</li>\n
+                    <li class = 'footer_li'>$Telefon</li>\n
+                    </ul>";
+        return $HTMLkod;
+}
