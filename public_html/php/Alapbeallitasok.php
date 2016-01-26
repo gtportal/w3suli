85e951e8e746f79cec7d61bacf77dffaa5fd3545
hw6 <hw6@Hw6>
2016-01-26 22:14:55 +0100

    getAlapbeallitasok() és getAlapbeallitasokForm()

--- public_html/php/Alapbeallitasok.php
+++ public_html/php/Alapbeallitasok.php
@@ -1,6 +1,4 @@
-<?php
-
-    $AlapAdatok               = array(); 
+<?php 
     $AlapAdatok['WebhelyNev'] = 'Webhely neve'; 
     $AlapAdatok['Iskola']     = '';
     $AlapAdatok['Cim']        = '';
@@ -9,17 +7,106 @@
         
 
     function setAlapbeallitasok() {  
-        trigger_error('Not Implemented!', E_USER_WARNING);
+    
+    global $MySqliLink, $AlapAdatok;
+    
+    if ($_SESSION['AktFelhasznalo'.'FSzint']>0) { // FSzint-et növelni, ha működik a felhasználókezelés!!!  
+		
+	$WNev        = $AlapAdatok['WebhelyNev'];
+	$Iskola      = $AlapAdatok['Iskola'];
+	$Cim         = $AlapAdatok['Cim'];
+	$Telefon     = $AlapAdatok['Telefon'];
+	$Stilus      = $AlapAdatok['Stilus'];		
+		
+    if (isset($_POST['WNev']))      {$WNev=test_post($_POST['WNev']);}  
+    if (isset($_POST['Iskola']))    {$Iskola=test_post($_POST['Iskola']);}  
+    if (isset($_POST['Cim']))   	{$Cim=test_post($_POST['Cim']);}  
+    if (isset($_POST['Telefon']))   {$Telefon=test_post($_POST['Telefon']);}  
+    if (isset($_POST['Stilus']))    {$Stilus=test_post($_POST['Stilus']);}  
+
+	$UpdateStr = "UPDATE AlapAdatok SET 
+				  WebhelyNev=$WNev,
+                  Iskola='$Iskola',
+                  Cim='$Cim',
+                  Telefon='$Telefon',
+                  Stilus='$Stilus' LIMIT 1";   
+                  
+     echo "<h1>".$UpdateStr."</h1>"; 
+                  
+    if (!mysqli_query($MySqliLink,$UpdateStr))  {echo "Hiba setAb 1";}   
+               
+	}
     }
 
 
     function getAlapbeallitasForm() {
-        trigger_error('Not Implemented!', E_USER_WARNING);
+		
+	global $AlapAdatok;
+	$HTMLkod='';
+	$ErrorStr = '';         
+
+
+	if ($_SESSION['AktFelhasznalo'.'FSzint']>0)  { // FSzint-et növelni, ha működik a felhasználókezelés!!!  
+		
+	$WNev        = $AlapAdatok['WebhelyNev'];
+	$Iskola      = $AlapAdatok['Iskola'];
+	$Cim         = $AlapAdatok['Cim'];
+	$Telefon     = $AlapAdatok['Telefon'];
+	$Stilus      = $AlapAdatok['Stilus'];
+
+
+	$HTMLkod .= "<div id='divAlapbeallitasForm' >\n";
+    if ($ErrorStr!='') {$HTMLkod .= "<p class='ErrorStr'>$ErrorStr</p>";}
+
+	$HTMLkod .= "<form action='?f0=alapbeallitasok' method='post' id='formAlapbeallitasForm'>\n";
+          	   
+    //Webhely neve
+    
+    $HTMLkod .= "<p class='pWNev'><label for='WNev' class='label_1'>A webhely neve:</label><br>\n ";
+	$HTMLkod .= "<input type='text' name='WNev' id='WNev' placeholder='Webhelynév' value='$WNev' size='40'></p>\n"; 
+	   
+    //Iskola neve
+    
+    $HTMLkod .= "<p class='pIskola'><label for='Iskola' class='label_1'>Az iskola neve:</label><br>\n ";
+	$HTMLkod .= "<input type='text' name='Iskola' id='Iskola' placeholder='Iskola neve' value='$Iskola' size='40'></p>\n"; 
+	   
+    //Iskola címe
+
+	$HTMLkod .= "<p class='pCim'><label for='Cim' class='label_1'>Az iskola címe:</label><br>\n ";
+	$HTMLkod .= "<input type='text' name='Cim' id='Cim' placeholder='Iskola címe' value='$Cim' size='40'></p>\n"; 
+	   
+    //Iskola telefonszám
+
+	$HTMLkod .= "<p class='pTelefon'><label for='Telefon' class='label_1'>Az iskola telefonszáma:</label><br>\n ";
+	$HTMLkod .= "<input type='text' name='Telefon' id='Telefon' placeholder='Iskola telefonszáma' value='$Telefon' size='40'></p>\n"; 
+   
+    //Stíluskiválasztó
+
+	$HTMLkod .= "<p class='pStilus'><label for='Stilus' class='label_1'>Stilus:</label>\n ";
+    $HTMLkod .= "<input type='number' name='Stilus' id='Stilus' min='0' max='10' step='1' value='$Stilus'></p>\n";  
+        
+    //Submit
+    $HTMLkod .= "<input type='submit' name='submitOldalForm' value='Módosítás'><br>\n";        
+    $HTMLkod .= "</form>\n";            
+    $HTMLkod .= "</div>\n";   
+               
+    return $HTMLkod;
+	}
     }
 
 
     function getAlapbeallitasok() {
-        trigger_error('Not Implemented!', E_USER_WARNING);
+		
+	global $MySqliLink;
+	$beallitasok = array();	
+		
+    $SelectStr   = "SELECT * FROM AlapAdatok LIMIT 1";     
+    $result      = mysqli_query($MySqliLink,$SelectStr) OR die("Hiba gAb 1"); 
+    $row    = mysqli_fetch_array($result, MYSQLI_ASSOC); mysqli_free_result($result);
+
+    return $row;
+
+
     }
 
     function getStyleClass() {
