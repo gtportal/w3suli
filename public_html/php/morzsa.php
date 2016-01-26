061af6fad796176546f83e1ea5c3ac68bb1b1ca7
hw6 <hw6@Hw6>
2016-01-26 09:20:53 +0100

    Morzsa kész

--- public_html/php/morzsa.php
+++ public_html/php/morzsa.php
@@ -18,12 +18,18 @@ function getMorzsaHTML() {
            
            //Ha az adott oldal vagy annak egy leszármazottja aktív, akkor leszármazottjait is megjelenítjük
            if ($Aktiv) {
+               if ($OID!=$Aktoldal['id'])
+               {$HTMLkod .= "<a href='?f0=$OURL' >$ONev</a>";}
                $HTMLkod .= Morzsa_Szint2($OID);
-               $HTMLkod .= "<a href='?f0=$OURL' >$ONev</a> | ";
            }
-           $HTMLkod .= "\n"; 
+            
         } 
-        if ($HTMLkod > '') {$HTMLkod = "<div class='Morzsa'>\n $HTMLkod  </div>\n";}
+        if ($HTMLkod > '') {
+            $HTMLkod = "<div class='Morzsa'>\n
+                        <a href='./' >Kezdőlap</a>
+                        $HTMLkod 
+                    
+                     </div>\n";}
         mysqli_free_result($result);  
         return $HTMLkod1.$HTMLkod;
     }
@@ -46,8 +52,9 @@ function getMorzsaHTML() {
                $OID==$DedSzuloId   
               ) {$Aktiv = true;} else {$Aktiv = false;}           
            //Ha az adott oldal vagy annak egy leszármazottja aktív, akkor leszármazottjait is megjelenítjük
-           if ($Aktiv) {                
-                $HTMLkod .= "<a href='?f0=$OURL' >$ONev</a> | ";
+           if ($Aktiv) {
+                if ($OID!=$Aktoldal['id'])
+               {$HTMLkod .= "<a href='?f0=$OURL' >$ONev</a>";}
                 $HTMLkod .= Morzsa_Szint3($OID);
            }
            $HTMLkod .= "\n";           
@@ -57,10 +64,30 @@ function getMorzsaHTML() {
     }
     
     
-    // NÉLKÜLE A HÍVÁSAKOR KIFEKSZIK A php
     function Morzsa_Szint3($OID) {
         global $Aktoldal, $SzuloOldal, $NagyszuloOldal, $MySqliLink, $DedSzuloId;
-        
-        
-    }    
+        $HTMLkod      = '';
+        //Harmadik szint 
+        $SelectStr   = "SELECT * FROM Oldalak WHERE OSzuloId=$OID order by ONev "; 
+        $result      = mysqli_query($MySqliLink,$SelectStr) OR die("Hiba OM 41 ");
+        while($row   = mysqli_fetch_array($result)) {
+            $ONev = $row['ONev']; $OURL = $row['OUrl']; $OID  = $row['id']; $OSzulo = $row['OSzuloId']; 
+           //Ha az adott oldal vagy annak első gyermeke aktív, akkor az 'AktLink' osztályba kerül
+           if ($OID==$Aktoldal['id']      || 
+               $OID==$SzuloOldal['id']    ||
+               $OID==$NagyszuloOldal['id'] ||
+               $OID==$DedSzuloId   
+              ) {$Aktiv = true;} else {$Aktiv = false;}           
+           //Ha az adott oldal vagy annak egy leszármazottja aktív, akkor leszármazottjait is megjelenítjük
+           if ($Aktiv) {
+                /*$SzOURL = $SzuloOldal['OURL'];
+                $SzONev = $SzuloOldal['ONev'];*/
+                if ($OID!=$Aktoldal['id'])
+               {$HTMLkod .= "<a href='?f0=$OURL' >$ONev</a>";}
+           }
+           $HTMLkod .= "\n";           
+        }
+        mysqli_free_result($result);  
+        return $HTMLkod;
+    }
 ?>
