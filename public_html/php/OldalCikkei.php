b2ce3dfc192fc841afc9bba4cd32b8f8a6f773d0
gtportal <gtportal@lm17gtportal>
2016-02-09 14:10:58 +0100

    Balázs: a Cikk.php előkészítve 2

--- public_html/php/OldalCikkei.php
+++ public_html/php/OldalCikkei.php
@@ -13,9 +13,17 @@
     }
 
 
-    function getCikkekForm($Oid) {
-        trigger_error('Not Implemented!', E_USER_WARNING);
-    }
+function getCikkekForm() {
+  $HTMLkod  = '';  
+    // Egyenlőre az összes, az oldalhoz tartozó cikket megjelenítjük, később lapozunk
+
+    
+  // Új cikk és
+  // a $_SESSION['SzerkCik'][id] és a $_SESSION['SzerkCik'][Oid] által meghatározott cikk kezelése 
+  $HTMLkod  = getUjCikkForm;
+  $HTMLkod  = getCikkForm();
+  $HTMLkod  = getCikkTorolForm();
+}
 
 
     function setCikkekLista($Oid) {
