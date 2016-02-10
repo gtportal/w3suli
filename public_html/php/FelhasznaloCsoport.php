632f86d7c632c632d61952eaaa38412ebfcc5562
gtportal <gtportal@lm17gtportal>
2016-02-10 09:00:43 +0100

    Gellért: a felhasználói csoportkezelés előkészítve

--- public_html/php/FelhasznaloCsoport.php
+++ public_html/php/FelhasznaloCsoport.php
@@ -5,34 +5,92 @@ $FelhasznaloCsopor['id']       = 0;
 $FelhasznaloCsopor['CsNev']    = '';
 $FelhasznaloCsopor['CsLeiras'] = '';
 
- 
-    function setUjFCsoport() {
-        trigger_error('Not Implemented!', E_USER_WARNING);
-    }
+function getCsoportValasztForm() {
+	// A felhasználóknál készített getFelhasznaloValasztForm() fgv-hez hasonló módon lehetővé teszi 
+	// a választást a létező közül
+	// A $_SESSION['SzerkFCsoport'] munkamenet változóban tároljuk az 
+	// aktuális csoport id-jét	
+}
 
-    function setFCsoport() {
-        trigger_error('Not Implemented!', E_USER_WARNING);
-    }
+function setCsoportValaszt() {
+	// I.) A $_SESSION['SzerkFCsoport'] munkamenet változó beállítása, ha
+	// a kiválasztó űrlapot elküdték
 
-    function setFCsoportTorol($CSid) {
-        trigger_error('Not Implemented!', E_USER_WARNING);
-    }
+	
+}
 
-    function getUjFCsoportForm() {
-        trigger_error('Not Implemented!', E_USER_WARNING);
-    }
 
-     function getFCsoportForm($CSid) {
-        trigger_error('Not Implemented!', E_USER_WARNING);
-    }    
+function initFCsoport() {
+    global $MySqliLink;
+    //Az oldalak táblában létrehzzuk a "Felhasználói csoportok" nevű oldalt
+    //Késöbb átkerül az Init.php-ba !!!!
+
+    $SelectStr   = "SELECT id FROM Oldalak WHERE OTipus=20 ";  //echo "<h1>$SelectStr</h1>";
+    $result      = mysqli_query($MySqliLink,$SelectStr) OR die("Hiba FCSi 01aa ");
+    $rowDB       = mysqli_num_rows($result); 
+    if ($rowDB == 0) {
+        //Kezdőlap létrehozása
+        $InsertIntoStr = "INSERT INTO Oldalak VALUES ('', 'Felhasználói csoportok','Felhasznaloi_csoportok',1,1,'Oldal leírása',
+                          'Oldal kulcsszavai',1,20,'Oldal tartalma','','')";        
+        if (!mysqli_query($MySqliLink,$InsertIntoStr)) {die("Hiba IO 01 ");} 
+    }
+}
+
+
+// ============= Új felhasználó csoport létrehozása ============  
+function setUjFCsoport() {
+    global $MySqliLink;
+    $ErrorStr = ''; 
+    initFCsoport();
+
+
+    return $ErrorStr;
+}
+
+function getUjFCsoportForm() {
+    global $MySqliLink;
+    $HTMLkod   = '';
     
-    function getFCsoportTorolForm($CSid) {
-        trigger_error('Not Implemented!', E_USER_WARNING);
-    }      
+    return $HTMLkod;
+}
+
+// ============= Felhasználó csoport adatainak módosítása ============     
+function setFCsoport() {
+    global $MySqliLink;
+    $ErrorStr = ''; 
     
-    function getCsoportListaHTML() {
-        trigger_error('Not Implemented!', E_USER_WARNING);
-    }
+    return $ErrorStr;
+}
+
+function getFCsoportForm() {
+    global $MySqliLink;
+    $HTMLkod   = '';
+    
+    return $HTMLkod;
+}    
+    
+// ============= Felhasználó csoport törlése ============  
+function setFCsoportTorol() {
+    global $MySqliLink;
+    $ErrorStr = ''; 
+    
+    return $ErrorStr;
+}
+
+function getFCsoportTorolForm() {
+    global $MySqliLink;
+    $HTMLkod   = '';
+    
+    return $HTMLkod;
+}      
+    
+// ============= Felhasználó csoport tagjainak listázása ============     
+function getCsoportListaHTML() {
+    global $MySqliLink;
+    $HTMLkod   = '';
+    
+    return $HTMLkod;
+}
 
 
 
