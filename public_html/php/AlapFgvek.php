d070313ba4569c367822af13ce04d5de3d67a850
gtportal <gtportal@lm17gtportal>
2016-06-16 13:19:00 +0200

    HEAD bővítés előkszítése

--- public_html/php/AlapFgvek.php
+++ public_html/php/AlapFgvek.php
@@ -1,6 +1,15 @@
 <?php
 
 
+function getURL(){
+    if(isset($_SERVER['HTTPS'])){
+        $protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https" : "http";
+    }
+    else{
+        $protocol = 'http';
+    }
+    return $protocol . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+}
 
 
 
