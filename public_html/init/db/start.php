da0e7e036f9562286efc115f1d8d62e6387646ea
gtportal <gtportal@gtportal-ssd>
2016-07-14 10:26:48 +0200

    Start.hph-ből az echo törlése

--- public_html/init/db/start.php
+++ public_html/init/db/start.php
@@ -1,5 +1,5 @@
 
-                      <?php echo "FSZINT: ".$_SESSION["AktFelhasznalo"."FSzint"];
+                      <?php 
                         if ($_SESSION["AktFelhasznalo"."FSzint"]>0) {
                           $MySqliLink = mysqli_connect('localhost', 'proba', 'proba', 'proba'); 
                           if (!$MySqliLink) { die('AB hiba 123'); 
