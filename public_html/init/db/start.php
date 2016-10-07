b4a82c0d1741e2a604dd494741e32a2bf1044f38
gtportal <gtportal@gtportal-ssd>
2016-10-07 19:32:14 +0200

    proba felhasználó kapcsolata

--- public_html/init/db/start.php
+++ public_html/init/db/start.php
@@ -2,7 +2,7 @@
                       <?php 
                         if ($_SESSION["AktFelhasznalo"."FSzint"]>0) {
                           $MySqliLink = mysqli_connect('localhost', 'proba', 'proba', 'proba'); 
-                          if (!$MySqliLink) { die('AB hiba 123'); 
-}
+                          if (!$MySqliLink) { die('AB hiba 123'); }
+                          if (!mysqli_set_charset($MySqliLink, "utf8")) {die('Kapcsolódási hiba 1 ') ;}
                         }    
                       ?>
